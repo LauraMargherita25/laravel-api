bd566749ff26fe4ac2615e34998e513606e8c745
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-29 21:04:27 +0200

    New page about us

--- resources/js/pages/AboutPage.vue
+++ resources/js/pages/AboutPage.vue
@@ -1,5 +1,14 @@
 <template>
-  
+  <div class="container">
+      <h1>About Us</h1>
+      <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim veritatis fuga culpa nobis placeat labore quas aliquid omnis aut a possimus beatae mollitia cum quibusdam, rerum consectetur sequi nesciunt nulla.
+          Nihil ipsam aliquid mollitia illo magnam a incidunt blanditiis corrupti dolores quos quidem suscipit inventore temporibus, hic perferendis fuga dolore voluptates consectetur amet? Beatae ea veritatis numquam error unde ab.
+          Iure provident magni, itaque minus non tenetur sunt ut, perferendis recusandae minima magnam commodi consectetur perspiciatis nulla, facilis dolores exercitationem odit ab laudantium sint quae. Dolorem consequatur ipsum dolorum tenetur?
+          Magnam numquam blanditiis quia quidem laudantium officia ipsa qui autem eius illum dolor quod, nihil officiis quae cum rerum itaque. Corrupti dolore praesentium fugit illo nesciunt quasi mollitia, ea labore?
+          Quae impedit ea nemo maxime, ducimus ad itaque vel exercitationem tempore iste in minus esse nostrum suscipit veritatis similique ipsam recusandae? Ipsam perferendis quas quasi facilis aliquid sed! Totam, expedita?
+      </div>
+  </div>
 </template>
 
 <script>
